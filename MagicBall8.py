14a3ce8ff830c5bcc245fcbaacd244a299308fce
imisi99 <isongrichard234@gmail.com>
2024-05-29 21:03:58 +0100

    Magic_ball8

---
+++ MagicBall8.py
@@ -0,0 +1,36 @@
+import random
+import time
+
+message = [
+    'It is certain',
+    'It is decidedly so',
+    'Yes definitely',
+    'Reply hazy try again',
+    'Ask again later',
+    'Concentrate and ask again',
+    'My reply is no',
+    'Outllook no so good',
+    'Very doubtful'
+]
+
+while True:
+    question = input('What question do you have for the wise one? ').lower()
+    if question == "":
+        response = 'Bruh be serious!'
+    elif question == 'nothing':
+        response = 'Then what are you doing here?'
+    else:
+        response = message[random.randint(0, len(message) - 1)]
+    time.sleep(1)
+    print(response)
+    time.sleep(2)
+    print('Do you have any other question(Y/N)')
+    reply = input().lower()
+    if reply == 'y':
+        continue
+    elif reply == 'n':
+        break
+    else:
+        print('Program exited due to invalid keyword')
+        time.sleep(1)
+        break
---
+++ list.py
@@ -0,0 +1,18 @@
+# x = int(input('how many cats do you have? '))
+# CatLists = []
+
+# for i in range (x):
+#     name = input(f'What is the name of cat{len(CatLists) + 1} ')
+#     CatLists.append(name)
+
+# print('The cat names are:')
+# for i in CatLists:
+#     print(' '+ i)
+
+
+# imisi = ['tall', 'fair', ['reading novels', 'solving problems with programming']]
+# height, complexion, hobbies = imisi
+# print(height)
+# print(complexion)
+# print(hobbies)
+
