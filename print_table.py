2b032855ca6edae0da5a5d2556ce20af6d44481b
imisi99 <isongrichard234@gmail.com>
2024-06-04 23:48:07 +0100

    PART1 project

--- print_table.py
+++ print_table.py
@@ -6,28 +6,16 @@ tableData = [
 
 
 def print_table(table):
-    max_length0 = 0
-    max_length1 = 0
-    max_length2 = 0
+    max_length = [0, 0, 0]
     for index, i in enumerate(table):
-        if index == 0:
-            for data in i:
-                length = len(data)
-                if length > max_length0:
-                    max_length0 = length
-        if index == 1:
-            for data in i:
-                length = len(data)
-                if length > max_length1:
-                    max_length1 = length
-        if index == 2:
-            for data in i:
-                length = len(data)
-                if length > max_length2:
-                    max_length2 = length
-    print(max_length2)
-    print(max_length1)
-    print(max_length0)
+        for data in i:
+            length = len(data)
+            if length > max_length[index]:
+                max_length[index] = length
+    for row in zip(*table):
+        print(list(zip(*table)))
+        print(row)
+        # print(' '.join([data.rjust(max_length[index]) for index, data in enumerate(row)]))
 
 
 print_table(tableData)
