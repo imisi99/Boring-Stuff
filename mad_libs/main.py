6632a0ba00ab1bd471893c6ef1d3ef7b610de82b
imisi99 <isongrichard234@gmail.com>
2024-06-09 21:51:30 +0100

    Mad_Libs

---
+++ mad_libs/main.py
@@ -0,0 +1,22 @@
+import re
+
+text = open('text.txt', 'r+')
+text_content = text.read()
+
+mad_libs = re.compile(r'ADJECTIVE|NOUN|VERB')
+change = mad_libs.findall(text_content)
+for i in change:
+    if i == 'ADJECTIVE':
+        enter = input('Enter an adjective: ')
+        text_content = text_content.replace('ADJECTIVE', f'{enter}', 1)
+    if i == 'VERB':
+        enter = input('Enter a verb: ')
+        text_content = text_content.replace(f'VERB', f'{enter}', 1)
+    if i == 'NOUN':
+        enter = input('Enter a noun: ')
+        text_content = text_content.replace(f'NOUN', f'{enter}', 1)
+
+print(text_content)
+
+text_content = ('There was once a ADJECTIVE boy that felt so motivated and tried to VERB but then hec VERB on a'
+                ' NOUN then cause a turmoil')
